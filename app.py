ee104e1c292a7471cbb31669af08983d5e6fcaaa
colab-acc <97726980+colab-acc@users.noreply.github.com>
2022-09-09 00:37:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Setting database types: Local/Remote
 ENV = 'dev'
-if ENV == 'prod':
+if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:anuragrai123@localhost/test_database'
 else:
